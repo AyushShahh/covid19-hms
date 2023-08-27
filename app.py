06674d87d7330e7dda4b7fac49cdd60755b1fe47
Ayush Shah <shahayush001@gmail.com>
2023-08-27 05:32:54 +0530

    add update function and add/update routes
    hospitals and medicines routes added and index route updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import data as d
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request
 
 
 db = d.db()
@@ -8,16 +8,57 @@ people = d.people(db)
 medicines = d.medicines(db)
 laboratory = d.laboratory(db)
 
-# Initialize flask app
+
 app = Flask(__name__)
 
-# Index route with GET and POST methods
+def update():
+    global hospitals
+    global people
+    global medicines
+    global laboratory
+    hospitals = d.hospitals(db)
+    people = d.people(db)
+    medicines = d.medicines(db)
+    laboratory = d.laboratory(db)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-
-    # When user reaches the page via post i.e., submitting the form
     if request.method == "POST":
         city = request.form.get("city").lower()
-        return render_template("index.html", data=[item for item in hospitals if item == city], show=True)
+        condition = request.form.get("condition").lower()
+        update()
+        return render_template("index.html", data=[item for item in hospitals if item[11] == city and (item[7] > 0 if condition == 'severe' else True)], show=True)
     else:
         return render_template("index.html")
+    
+@app.route("/hospitals", methods=["GET", "POST"])
+def hospital():
+    if request.method == "POST":
+        hospital_id = int(request.form.get("id"))
+        t_nbeds = int(request.form.get("t_nbeds")) if request.form.get("t_nbeds") else 0
+        a_nbeds = int(request.form.get("a_nbeds")) if request.form.get("a_nbeds") else 0
+        t_vbeds = int(request.form.get("t_vbeds")) if request.form.get("t_vbeds") else 0
+        a_vbeds = int(request.form.get("a_vbeds")) if request.form.get("a_vbeds") else 0
+        staff = int(request.form.get("staff")) if request.form.get("staff") else 0
+        oxygen = int(request.form.get("oxygen")) if request.form.get("oxygen") else 0
+        nebulizer = int(request.form.get("nebulizer")) if request.form.get("nebulizer") else 0
+        d.update_hospital(db, hospital_id, t_nbeds, a_nbeds, t_vbeds, a_vbeds, staff, oxygen, nebulizer)
+        update()
+
+        chospital = [hospital for hospital in hospitals if hospital[0] == hospital_id]
+        if chospital:
+            return render_template("hospital.html", data=chospital[0], show=True)
+        else:
+            return redirect("hospital.html")
+    else:
+        return render_template("hospital.html")
+
+@app.route("/medicines", methods=["GET", "POST"])
+def medic():
+    if request.method == "POST":
+        city = request.form.get("city").lower()
+        d = [medicine for medicine in medicines if medicine[2] == city]
+        show = True if d else False
+        return render_template("medicine.html", data=d, show=show)
+    else:
+        return render_template("medicine.html")
